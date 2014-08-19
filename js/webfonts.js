1856b6eeb54d5b259240dd88928f7ddfd7980956
João Doria <dev@joaodoria.com>
2014-08-19 14:49:55 +0200

    adding tracking .js for webfonts

---
+++ js/webfonts.js
@@ -0,0 +1,8 @@
+var MTIProjectId='6ba70362-49fa-45c0-a934-19a0771ac4c5';
+ (function() {
+        var mtiTracking = document.createElement('script');
+        mtiTracking.type='text/javascript';
+        mtiTracking.async='true';
+        mtiTracking.src=('https:'==document.location.protocol?'https:':'http:')+'//fast.fonts.net/t/trackingCode.js';
+        (document.getElementsByTagName('head')[0]||document.getElementsByTagName('body')[0]).appendChild( mtiTracking );
+   })();
